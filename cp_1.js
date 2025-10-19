6792865c99a126fb053aec1879cd3402e3a7a548
Minh Anh Nguyen <minhanhn@usf.edu>
2025-10-19 13:51:41 -0400

    Counted characters length using addEventListener  on js file

--- cp_1.js
+++ cp_1.js
@@ -1,5 +1,17 @@
 // Step 3 - Use DOM event listeners
 // Count characters as the user types (use input or keydown event)
 const nameInput = document.getElementById("userName");
-const emailInput = document.querySelector("#email");
-const commentsInput = document.querySelector("#comments");
+const emailInput = document.getElementById("email");
+const commentsInput = document.getElementById('comments');
+
+commentsInput.addEventListener("input", () => {
+    console.log("Characters:", commentsInput.value.length)
+});
+
+emailInput.addEventListener("input", () => {
+    console.log("Characters:", emailInput.value.length)
+} );
+
+nameInput.addEventListener("input", () =>{
+    console.log("Characters:", nameInput.value.length)
+});
