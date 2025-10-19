b0a24ee37de08cef3c7448d04bae553b50c2fc59
Minh Anh Nguyen <minhanhn@usf.edu>
2025-10-19 13:38:57 -0400

    Selected elements on js file

---
+++ cp_1.js
@@ -0,0 +1,5 @@
+// Step 3 - Use DOM event listeners
+// Count characters as the user types (use input or keydown event)
+const nameInput = document.getElementById("userName");
+const emailInput = document.querySelector("#email");
+const commentsInput = document.querySelector("#comments");
